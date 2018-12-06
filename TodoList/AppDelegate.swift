96df234aec4e11ee5254fa157fa638fe7c2cc4f2
Nick Zolondek <nick.zolondek@gmail.com>
2018-12-05 21:54:37 -0600

    Initial Commit

---
+++ TodoList/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TodoList
+//
+//  Created by Nick Zolondek on 12/5/18.
+//  Copyright © 2018 Nick Zolondek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TodoList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Nick Zolondek on 12/5/18.
+//  Copyright © 2018 Nick Zolondek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
